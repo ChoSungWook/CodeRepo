f28edd1bfc9a3cd979a7f7c9c1a4747d9e8e378c
ChoSungWook <fldrh224728@ztacom.com>
2021-07-01 18:20:11 +0900

    dataMonitoringServer Tag기능 추가

--- dataMonitoringServer/public/app.js
+++ dataMonitoringServer/public/app.js
@@ -3,6 +3,7 @@
     var dataObject = new Object();
     var dataList = new Array();
     var initializedFlag = false;
+    var auth = 1; // debug: -1 admin 0 normal: 1  
 
 
   
@@ -40,18 +41,47 @@
         }
         initializedFlag = true;
         liItemInit();
-
-        console.log(pwValue.value);
+        var pw = pwValue.value;
+        console.log(pw);
+        if(pw==='admin')
+            auth = 0;
+        
         pwValue.disabled = true;
     }
 
 
-  
+  var tagList = new Array();
     function getDataText(data)
     {
         var datDiv = document.createElement('div');
         datDiv.style['margin-left'] = '10px';
         var dataSizeText = 'data-size : '+data.length;
+        var tagdata = "tag:\r\n";
+        if(auth == 0)
+        {
+          var tagText = document.getElementById('tagText');//.textContent.slice(',');
+          tagList = tagText.value.split(',');
+          var jsonProperty;
+          if(data[0]=='{' && data[data.length-1]=='}')
+          {
+            console.log("json data");
+            data = data.slice(1,data.length-1);
+            jsonProperty = data.split(',');
+            var i=0,j=0;
+            for(i=0;i<tagList.length;++i)
+            {
+              var idx = jsonProperty.findIndex(data => data.includes(tagList[i]+":"));
+              if(idx!=-1)
+              {
+                tagdata+= jsonProperty[idx]+"\r\n";
+                data = data.slice(0,idx)+data.slice(jsonProperty[idx].length+1,data.length-1);
+              }
+            }
+
+          }
+          datDiv.appendChild(document.createTextNode(tagdata));
+          datDiv.appendChild(document.createElement('br'));
+        }
 
         var dataText='data : '+data;
         datDiv.appendChild(document.createTextNode(dataSizeText));
@@ -109,19 +139,71 @@
 
         }
     }
-    /*document.getElementById('funcBtn').onclick=
+
+//====================================================================
+// Block. Menu Button listener
+//====================================================================
+var menu_list = ['menu_liveData','menu_settings'];
+function clearSelectItem()
+{
+  var i=0;
+  for(i=0;i<menu_list.length;++i)
+  {
+    var clist = document.getElementById(menu_list[i]);
+    if(clist.classList.contains('selected-menu-label'))
+      {
+          clist.classList.remove('selected-menu-label');
+          clist.classList.add('menu-label');
+      } 
+
+  }
+}
+function selectMenu(id)
+{
+  var menu_id = document.getElementById(id);
+  clearSelectItem();
+  menu_id.classList.remove('menu-label');
+  menu_id.classList.add('selected-menu-label');
+}
+document.getElementById('menu_liveData').onclick=function(){  
+  selectMenu('menu_liveData');
+  document.getElementById('dataList').style.display = '';
+  document.getElementById('settingSec').style.display='none';
+}
+document.getElementById('menu_settings').onclick=function(){
+    selectMenu('menu_settings');
+    document.getElementById('dataList').style.display = 'none';
+    document.getElementById('settingSec').style.display='';
+
+    if(auth == 1)
+    {
+      document.getElementById('tagFunc').style.display = 'none';
+    }
+    else
+    {
+      document.getElementById('tagFunc').style.display = '';
+
+    }
+}
+////
+
+    document.getElementById('funcBtn').onclick=
     function()
     {
-        console.log("function button clicked");
-        if(initializedFlag == 1){
+      console.log("function button clicked");
+      console.log(document.getElementById('dataList').style.display);
+        
+        if(initializedFlag == 1)
+          dataObject.addData('TestData');
+        /*if(initializedFlag == 1){
             var url = "/api/data";
             ajaxGrant(url, "GET", {} , {}, onGetScanDataSuccess,onGetScanDataFail);
           }
           else {
             console.log("any client is not opened");
-          }
+          }*/
     }
-*/
+
    
     setInterval(function()
     {
@@ -138,11 +220,15 @@
  {
      response = JSON.parse(response);
      console.log('res.len :: '+response.length);
+     
      var i =0;
      for(i=0;i<response.length;++i)
      {
          if(typeof(response[i])=='object')
-            dataObject.addData(JSON.stringify(response[i]));
+         {
+           if(response[i].length!=0)
+              dataObject.addData(JSON.stringify(response[i]));
+          }
          else
             dataObject.addData(response[i].toString());
      }
